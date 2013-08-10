6b878d37336b0a1cb2caacdfd354d24d3205b348
David Brear <davidbrear04@gmail.com>
2013-08-10 16:47:30 -0500

    paypal requests are successfully firing but not method for kicking back success or not

--- package.js
+++ package.js
@@ -6,5 +6,5 @@ Npm.depends({'paypal-rest-sdk': '0.6.3'});
 
 Package.on_use(function(api, where){
   api.use(['templating', 'npm'], ['client', 'server']);
-  api.add_files(['paypal.js', 'lib/paypal.js'], ['client', 'server']);
+  api.add_files(['paypal.js', './lib/paypal.js'], ['client', 'server']);
 });
--- paypal.js
+++ paypal.js
@@ -1,36 +1,50 @@
 //we don't want to store this information on the server
-Meteor.transaction = new Meteor.Collection(null);
+Meteor.transactions = new Meteor.Collection(null);
 
 Meteor.Paypal = {
-  authorize: function(card_info, callback){
-    Meteor.call('paypal_submit', card_info, function(err){
-      console.log('done with the submission');
+  authorize: function(card_info, payment_info, callback){
+    Meteor.call('paypal_submit', card_info, payment_info, function(err, payment){
+      if(err){
+        console.err(err)
+      } else {
+        console.log('done with the submission');
+      }
     });
   },
   config: function(options){
     this.account_options = options;
+  },
+  parseCardData: function(data){
+    var card_data = {
+      credit_card: {
+        type: 'visa',
+        number: data.number,
+        first_name: data.name.split(' ')[0],
+        last_name: data.name.split(' ')[1],
+        cvv2: data.cvv2,
+        expire_month: data.expire_month,
+        expire_year: data.expire_year
+      }};
+    this.payment_json.payer.funding_instruments.push(card_data);
+  },
+  parsePaymentData: function(data){
+    var payment_data = {amount: {total: data.total, currency: data.currency}};
+    this.payment_json.transactions.push(payment_data);
   }
 };
 
-var parsePaymentData = function(data){
-  var card_data = {type: 'visa', first_name: data.name.split(' ')[0], last_name: data.name.split(' ')[1], cvv2: data.cvv2, expire_month: data.expire_month, expire_year: data.expire_year};
-  console.log(card_data);
-  paypal_json.payer.funding_instruments.push(card_data);
-};
-
-var paypal_sdk = undefined;
 if(Meteor.isServer){
-  paypal_sdk = Npm.require('paypal-rest-sdk');
+  var paypal_sdk = Npm.require('paypal-rest-sdk');
+  Meteor.methods({
+    paypal_submit: function(cardData, paymentData){
+      if(typeof(paypal_sdk) === 'undefined')
+        return;
+      var config = Meteor.Paypal.account_options;
+      paypal_sdk.configure(config);
+      Meteor.Paypal.parseCardData(cardData);
+      Meteor.Paypal.parsePaymentData(paymentData);
+      paypal_sdk.payment.create(Meteor.Paypal.payment_json, function(err, payment){
+      });
+    }
+  });
 }
-Meteor.methods({
-  paypal_submit: function(paymentData){
-    if(typeof(paypal_sdk) === 'undefined')
-      return;
-    var config = Meteor.Paypal.account_options;
-    parsePaymentData(paymentData);
-    paypal_sdk.payment.create(paypal_json, function(err, payment){
-      console.log('err: ' + err);
-      console.log('payment: ' + payment);
-    });
-  }
-});
