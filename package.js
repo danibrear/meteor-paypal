d814d7f6536e31d51faaac605af8e4a6b25d9255
Lalo Martins <lalo.martins@gmail.com>
2014-01-17 14:29:56 +0100

    npm package is no longer needed or welcome

--- package.js
+++ package.js
@@ -6,7 +6,6 @@ Npm.depends({'paypal-rest-sdk': '0.6.3'});
 
 Package.on_use(function(api){
   api.use('templating', 'client');
-  api.use( 'npm', ['client', 'server']);
   api.add_files('paypal.js', ['client', 'server']);
   api.add_files(['paypal_credit_card_form.html', 'paypal_credit_card_form.js'], 'client');
   api.export('Paypal', ['client', 'server']);
